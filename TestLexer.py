1ae72c47a8b8d93d059f573167e89c9901b29b36
Homayoonn <homayoon.9171@gmail.com>
2020-12-03 14:38:58 +0330

    Fix Some Code again

--- TestLexer.py
+++ TestLexer.py
@@ -3,28 +3,8 @@ from DecafLexerer import DecafLexerer
 
 _FILENAME = 'sample.script'
 
-# # Function to convert number into string
-# # Switcher is dictionary data type here
-# def numbers_to_strings(argument):
-# 	switcher = {
-# 		0: "zero",
-# 		1: "one",
-# 		2: "two",
-# 	}
-#
-# 	# get() method of dictionary data type returns
-# 	# value of passed argument if it is present
-# 	# in dictionary otherwise second argument will
-# 	# be assigned as default value of passed argument
-# 	return switcher.get(argument, "nothing")
-#
-# # Driver program
-# if __name__ == "__main__":
-# 	argument=0
-# 	print (numbers_to_strings(argument))
 
-
-def get_Token_Type(token_type):
+def getTokenType(token_type):
     switcher = {
         DecafLexerer.STRING: "STRING",
         DecafLexerer.LPAREN: "LPAREN",
@@ -44,7 +24,7 @@ def main():
         i = 0
         while token.type != Token.EOF:
             print("[{}] {}   {}".format(
-                i, get_Token_Type(token.type), token.text)
+                i, getTokenType(token.type), token.text)
             )
             token = lexer.nextToken()
             i += 1
