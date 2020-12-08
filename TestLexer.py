75070c2b8800d7cb0ac77b31fed970c6ece5c348
Homayoonn <homayoon.9171@gmail.com>
2020-12-08 15:00:45 +0330

    Fix EveryThings

--- TestLexer.py
+++ TestLexer.py
@@ -10,11 +10,12 @@ def getTokenType(token_type):
         DecafLexerer.T_DOUBLE: "T_DOUBLE",
         DecafLexerer.T_BOOLEAN: "T_BOOLEAN",
         DecafLexerer.T_ID: "T_ID",
+        DecafLexerer.T_HEX: "T_HEX",
         DecafLexerer.T_STRING: "T_STRING",
         DecafLexerer.T_KEY_WORDS: "T_KEY_WORDS",
         DecafLexerer.T_PUNCTUATION: "T_PUNCTUATION"
     }
-    return switcher.get(token_type, "OTHER")
+    return switcher.get(token_type, "UNDEFINED_TOKEN")
 
 
 def main():
@@ -22,11 +23,16 @@ def main():
         input_stream = InputStream(reader.read())
         lexer = DecafLexerer(input_stream)
         token = lexer.nextToken()
+        valid_token = ["T_ID", "T_STRING", "T_BOOLEAN", "T_DOUBLE", "T_INT"]
         i = 0
         while token.type != Token.EOF:
-            print("[{}] {}   {}".format(
-                i, getTokenType(token.type), token.text)
-            )
+            # print("[{}] {}   {}".format(
+            #     i, getTokenType(token.type), token.text)
+            # )
+            if getTokenType(token.type) in valid_token:
+                print(getTokenType(token.type), token.text)
+            else:
+                print(token.text)
             token = lexer.nextToken()
             i += 1
 
